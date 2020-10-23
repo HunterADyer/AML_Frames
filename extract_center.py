1531b6d812dd14de81bde2f3c02fd9cd489760f7
Hunter Dyer <hunter.a.dyer@gmail.com>
2020-10-23 17:46:20 -0500

    fixed bug, now has 256/256 images

--- extract_center.py
+++ extract_center.py
@@ -9,10 +9,9 @@ for folder in folders:
     if not os.path.isdir(output_directory+'/'):
         os.mkdir(output_directory)
     for image in os.listdir(folder):
-        print(folder + '/' + image)
         img = cv2.imread(folder + '/' + image, cv2.IMREAD_UNCHANGED)
         cent_x = len(img)//2
         cent_y = len(img[0])//2
-        cent_cut = img[cent_x-128:cent_x+128][cent_y-128:cent_y+128]
+        cent_cut = img[cent_x-128:cent_x+128,cent_y-128:cent_y+128]
 
         cv2.imwrite(output_directory + '/' + image, cent_cut)
