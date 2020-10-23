2c72485109bcabc50cbdc76f9021d4e301a799e2
Hunter Dyer <hunter.a.dyer@gmail.com>
2020-10-23 17:33:47 -0500

    extracted center

---
+++ extract_center.py
@@ -0,0 +1,18 @@
+import sys
+import os
+import cv2
+
+folders = sys.argv[1:]
+
+for folder in folders:
+    output_directory = folder + '_cent'
+    if not os.path.isdir(folder+'/'):
+        os.mkdir(output_directory)
+    for image in os.listdir(folder):
+        print(folder + '/' + image)
+        img = cv2.imread(folder + '/' + image, cv2.IMREAD_UNCHANGED)
+        cent_x = len(img)//2
+        cent_y = len(img[0])//2
+        cent_cut = img[cent_x-128:cent_x+128][cent_y-128:cent_y+128]
+
+        cv2.imwrite(output_directory + '/' + image, img)
